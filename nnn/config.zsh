1a6aa2408f981517c47b4cdf38e32364d9df28d1
Filip-Groh <filip.groh.023@pslib.cz>
2025-11-03 17:58:47 +0100

    chore: Sync nnn config on

---
+++ nnn/config.zsh
@@ -0,0 +1,18 @@
+n ()
+{
+    [ "${NNNLVL:-0}" -eq 0 ] || {
+        echo "nnn is already running"
+        return
+    }
+
+    export NNN_TMPFILE="${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
+    export NNN_FIFO="/tmp/nnn.fifo"
+    export NNN_PLUG='p:preview-tui'
+
+    command nnn -Ha -P p "$@"
+
+    [ ! -f "$NNN_TMPFILE" ] || {
+        . "$NNN_TMPFILE"
+        rm -f -- "$NNN_TMPFILE" > /dev/null
+    }
+}
