e23d99f670fcfbfe4c719bc87abac6765d630bdd
Filip-Groh <filip.groh.023@pslib.cz>
2026-08-14 22:34:25 +0200

    chore: Sync nvim config on

--- nvim/init.lua
+++ nvim/init.lua
@@ -120,6 +120,10 @@ end)
 
 -- Enable break indent
 vim.o.breakindent = true
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
 
 -- Save undo history
 vim.o.undofile = true
